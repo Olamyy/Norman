63119123e4217ae072e7d6d2f546f880269ec363
Olamilekan Wahab <olamyy53@gmail.com>
2017-03-10 00:48:17 +0100

    echo bot update

--- Norman/api/bot.py
+++ Norman/api/bot.py
@@ -46,62 +46,22 @@ class WebHook(Resource):
         else:
             return response.response_error('Failed validation. Make sure the validation tokens match', args)
     
-    def post(self):
-        data = request.get_json()
-        print(data)
-        if data.get('object', None) == 'page':
-            message_entries = data['entry']
-            for entry in message_entries:
-                # page_id = entry['id']
-                # time_of_event = entry['time']
-                for event in entry['messaging']:
-                    if event.get('message', None):
-                        self.reply(event)
-
-    def reply(self, event):
-        sender_id = event['sender']['id']
-        # recipient_id = event['recipient']['id']
-        # time_of_message = event['timestamp']
-        message = event['message']
-
-        # message_id = message['mid']
-        message_text = message.get('text', None)
-        message_attachments = message.get('attachments', None)
-
-        if message_text:
-            if message_text == 'generic':
-                self.send_generic_message(sender_id)
-            else:
-                self.send_text_message(sender_id, message_text)
-        elif message_attachments:
-            print("message with attachment received")
-            self.send_text_message(sender_id)
-
-    def send_generic_message(self, recipient_id):
-        generic_greeting = "Hello, I'm Norman, your personal assistant\
-        I help you keep track of your health."
-        message_data_dict = {'recipient': {'id': recipient_id}, 'message': {'text': generic_greeting}}
-        message_data = json.dumps(message_data_dict)
-        self.call_send_api(message_data)
-
-    def send_text_message(self, recipient_id, message_text=None):
-        message_data_dict = {'recipient': {'id': recipient_id}, 'message': {'text': message_text}}
-        message_data = json.dumps(message_data_dict)
-        self.call_send_api(message_data)
-
-    @staticmethod
-    def call_send_api(message):
-        access_token = 'EAAS0PtgoBk4BAAV6pDrocKGlPOAjdxyn' \
-                       'xBidP5noah1l27yRu2x0zZAc' \
-                       '1clvjQN1YAY9dHgJKGd8fPqjwHB' \
-                       'xQ0KuQxUahCkxUDmecH9OQvAk8FKuZBZA2jQhHPihh85WcALD8UZBMRxYXT1iAqaAGdpUcjKAlOBwL6VwisVC8QZDZD'
-        uri = 'https://graph.facebook.com/v2.6/me/messages/access_token=' + access_token
-        try:
-            resp = r.post(uri, json=message)
-            print(resp.status_code, '\n')
-            print(resp.text)
-        except r.ConnectionError:
-            print('Unable to connect to graph api')
+    def post():
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for x in messaging:
+                if x.get('message'):
+                    recipient_id = x['sender']['id']
+                    if x['message'].get('text'):
+                        message = x['message']['text']
+                        bot.send_text_message(recipient_id, message)
+                    if x['message'].get('attachments'):
+                        for att in x['message'].get('attachments'):
+                            bot.send_attachment_url(recipient_id, att['type'], att['payload']['url'])
+                else:
+                    pass
+        return "Success"
 
 
 @blueprint.route('/test-mongo', methods=['POST'])
